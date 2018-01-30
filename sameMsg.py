7e12f193d5e6dd26185f030a6dbcc731e60f70e0
Malhotraguy <r_malhot@encs.concordia.ca>
2018-01-30 12:37:32 -0500

    Update sameMsg.py

--- sameMsg.py
+++ sameMsg.py
@@ -3,8 +3,8 @@ numbers=["+15148349361","+15143497155"]
 for i in range(2):
     from twilio.rest import Client
 
-    account_sid = "ACddc59696c52e6d717778a4621d700dc1"
-    auth_token = "c324cdc7ba3923cb9b832d7f9975427a"
+    account_sid = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    auth_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
     client = Client(account_sid, auth_token)
 
